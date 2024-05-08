6e261dbd5bd474b68bc7312dd4d53fe65b021e06
Danny Teok <43955027+dteok@users.noreply.github.com>
2024-05-08 22:22:06 +0100

    refactor: request blocks moved to be part of for fruit_chosen code block (#3)
    Lesson 10: Using Fruityvice Data More Effectively.

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,14 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + " "
 
+        # New section to display fruityvice nutrition information
+        fruityvice_response = requests.get(
+            "https://fruityvice.com/api/fruit/watermelon", timeout=30
+        )
+        fv_dataframe = st.dataframe(
+            data=fruityvice_response.json(), use_container_width=True
+        )
+
     # st.write(ingredients_string)
 
     my_insert_stmt = (
@@ -48,7 +56,3 @@ if ingredients_list:
             st.success(
                 "Thank you, " + name_on_order + ", your Smoothie is ordered!", icon="✅"
             )
-# New section to display fruityvice nutrition information
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_dataframe = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
